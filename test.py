c4a27fa8bad91c6a89a92bc15ba2dabdb3fb107b
unknown <2910anshul1997@gmail.com>
2020-04-23 15:31:56 +0530

    Bug Fixes

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from lxml import html
 import requests
-import credentials #
+import credentials #create a file named credentials.py which stores credentials as username = "" and password= ""
 
-browser = webdriver.Chrome("C:\\Users\\Anshul\\Desktop\\Instagram\\chromedriver_win32\\chromedriver.exe")
+
+browser = webdriver.Chrome("C:\\Users\\Anshul\\Desktop\\Instagram\\chromedriver_win32\\chromedriver.exe") #Chrome WebDriver
 browser.maximize_window()
-# browser = webdriver.Edge("C:\\Users\\Anshul\\Desktop\\Instagram\\edgedriver_win64\\msedgedriver.exe")
+# browser = webdriver.Edge("C:\\Users\\Anshul\\Desktop\\Instagram\\edgedriver_win64\\msedgedriver.exe")   #Edge WebDriver
 # time.sleep(1)
+
 link = 'https://instagram.com/' + credentials.username
 browser.get("https://www.instagram.com/accounts/login/?hl=en&source=auth_switcher")
 time.sleep(1)
@@ -32,9 +34,10 @@ soup = BeautifulSoup(page.text, "html.parser")
 for i in soup.find_all(name="meta", attrs={"property": "og:description"}):
         rawData = i['content'].split()
         
-with open ("soup.txt", "w") as file:
+with open ("soup.txt", "w") as file: #saving source code of target's profile to scrape the details.
         file.write(str(soup))
 
+# print(rawData) #details in raw format (list)
 
 followers = rawData[0] + " Followers"
 following = rawData[2] + " Following"
